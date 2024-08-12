03ccf9cadfdfe34f4c88901688335a35d955cac6
ghulammurtazakhan97@gmail.com <ghulammurtazakhan@gmail.com>
2024-08-13 00:13:03 +0500

    frontend hero section done

---
+++ main.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('wallet'); 
+    const accountNumber = document.getElementById('accountNumber'); 
+    if (loginButton) {
+      loginButton.addEventListener('click', async () => {
+        if (typeof window.ethereum !== 'undefined') {
+          try {
+            // Request account access if needed
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const account = accounts[0];
+            if(account){
+                accountNumber.innerHTML = `${account}`;
+                accountNumber.style.fontSize = "15px";
+            }
+            alert(`Connected: ${account}`);
+  
+            // Perform additional authentication logic if needed
+          } catch (error) {
+            alert(`Error: ${error.message}`);
+          }
+        } else {
+          alert('MetaMask is not installed. Please install it to use this application.');
+        }
+      });
+    } else {
+      alert('Login button not found');
+    }
+  });
+
+  function scrollToNextSection() {
+    // Get the current scroll position
+    const currentScrollPosition = window.scrollY;
+    // Get all sections
+    const sections = document.querySelectorAll('section');
+    // Loop through each section
+    for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        // Get the position of the section
+        const sectionTop = section.offsetTop;
+        // Check if the current scroll position is less than the top of the section
+        if (currentScrollPosition < sectionTop - 1) {
+            // Scroll to the section
+            window.scrollTo({
+                top: sectionTop,
+                behavior: 'smooth'
+            });
+            break;
+        }
+    }
+}
+  
